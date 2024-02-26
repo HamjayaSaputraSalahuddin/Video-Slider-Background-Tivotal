25273014d81ff1dab4943e0d439ce444c41144ab
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-26 11:22:54 +0800

    add javascript code for buttons and fix bugs

---
+++ app.js
@@ -0,0 +1,22 @@
+let menuBton = document.querySelector(".menu-btn");
+let header = document.querySelector(".header");
+let btns = document.querySelectorAll(".nav-item");
+let videos = document.querySelectorAll("video");
+
+menuBton.addEventListener("click", () => {
+    header.classList.toggle("active");
+});
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        btns.forEach((item) => {
+            item.classList.remove("active");
+        });
+        videos.forEach((video) => {
+            video.classList.remove("active");
+        });
+
+        videos[i].classList.add("active");
+        btn.classList.add("active");
+    });
+});
